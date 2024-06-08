cd5358837d9c52485765abe7c491a0cc0818b9e4
Pratyush <pratyushghatole2003@gmail.com>
2024-06-08 22:09:42 +0530

    TRZ

---
+++ src/Components/GitProfile.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import GitHub from 'github-api';
+import { GraphQLClient, gql } from 'graphql-request';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import 'react-calendar-heatmap/dist/styles.css';
+
+const UserComponent = ({ username }) => {
+    const [user, setUser] = useState(null);
+    const [contributions, setContributions] = useState([]);
+    const [error, setError] = useState(null);
+    const [contributionsThisYear, setContributionsThisYear] = useState(0);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const gh = new GitHub();
+                const user = gh.getUser(username);
+                const { data } = await user.getProfile();
+                setUser(data);
+            } catch (err) {
+                setError('User not found');
+            }
+        };
+
+        const fetchContributions = async () => {
+            const endpoint = 'https://api.github.com/graphql';
+            const token = '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';
+            const client = new GraphQLClient(endpoint, {
+                headers: {
+                    authorization: `Bearer ${token}`,
+                },
+            });
+
+            const query = gql`
+                query($username: String!) {
+                    user(login: $username) {
+                        contributionsCollection {
+                            contributionCalendar {
+                                totalContributions
+                                weeks {
+                                    contributionDays {
+                                        date
+                                        contributionCount
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            `;
+
+            const variables = { username };
+
+            try {
+                const data = await client.request(query, variables);
+                const weeks = data.user.contributionsCollection.contributionCalendar.weeks;
+                const contributions = weeks.flatMap(week => week.contributionDays);
+                setContributions(contributions);
+                setContributionsThisYear(data.user.contributionsCollection.contributionCalendar.totalContributions);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        fetchUser();
+        fetchContributions();
+    }, [username]);
+
+    const classForValue = value => {
+        if (!value) {
+            return 'color-empty';
+        }
+        return value.count > 0 ? 'color-active' : 'color-inactive';
+    };
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className='heatmap w-full text-white flex flex-col gap-4 text-center relative'>
+            <div className='gitbtn flex w-1/2 m-auto justify-evenly gap-4 items-center'>
+                <h2>Contributions: {contributionsThisYear}</h2>
+            </div>
+            <CalendarHeatmap
+                startDate={new Date(new Date().setFullYear(new Date().getFullYear() - 1))}
+                endDate={new Date()}
+                values={contributions.map(day => ({
+                    date: day.date,
+                    count: day.contributionCount,
+                }))}
+                classForValue={classForValue}
+            />
+        </div>
+    );
+
+};
+
+export default UserComponent;
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import StackIcon from "tech-stack-icons";
+import Profile from './GitProfile';
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 
 
 const skills = [
   { name: 'HTML', icon: <StackIcon name="html5" /> },
   { name: 'CSS', icon: <StackIcon name="css3" /> },
   { name: 'JavaScript', icon: <StackIcon name="js" /> },
-  { name: 'React', icon: <StackIcon name="reactjs" />},
-  { name: 'Bootstrap', icon: <StackIcon name="bootstrap5" />},
-  { name: 'Tailwind', icon: <StackIcon name="tailwindcss" />},
+  { name: 'React', icon: <StackIcon name="reactjs" /> },
+  { name: 'Bootstrap', icon: <StackIcon name="bootstrap5" /> },
+  { name: 'Tailwind', icon: <StackIcon name="tailwindcss" /> },
   { name: 'Node.js', icon: <StackIcon name="nodejs" /> },
   { name: 'FireBase', icon: <StackIcon name="firebase" /> },
   { name: 'MongoDb', icon: <StackIcon name="mongodb" /> },
   { name: 'mySql', icon: <StackIcon name="mysql" /> },
+  { name: 'Github', icon: <StackIcon name="github" className='bg-white rounded-lg' /> },
+  { name: 'Git', icon: <StackIcon name="git" /> },
+  { name: 'VScode', icon: <StackIcon name="vscode" /> },
+  { name: 'Render', icon: <StackIcon name="render" className='bg-white rounded-3xl' /> },
+  { name: 'Netlify', icon: <StackIcon name="netlify" /> },
+  { name: 'Clodinary', icon: <StackIcon name="cloudinary" /> },
+  { name: 'Figma', icon: <StackIcon name="figma" /> },
+  { name: 'Postman', icon: <StackIcon name="postman" /> },
 ];
 
-const tools = [
-  { name: 'Github', icon: <StackIcon name="github" className='bg-white rounded-lg'/>},
-  { name: 'Git', icon: <StackIcon name="git"/>},
-  { name: 'VScode', icon: <StackIcon name="vscode" /> },
-  { name: 'Render', icon: <StackIcon name="render" className='bg-white rounded-3xl'/>},
-  { name: 'Netlify', icon: <StackIcon name="netlify" />},
-  { name: 'Postman', icon: <StackIcon name="postman" />},
-  { name: 'Canva', icon: <StackIcon name="canva" /> },
-  { name: 'Figma', icon: <StackIcon name="figma" />},
-  { name: 'Photoshop', icon: <StackIcon name="ps" />},
-  { name: 'Cloudinary', icon: <StackIcon name="cloudinary" />},
-];
 
 
 
@@ -35,23 +34,42 @@ const Skills = () => {
     <section className="skills-section text-white">
       <div className="skills-container">
         <div className='skill-left'>
-        <h1 className='skillTitle about-title'>Dev-Skills</h1>
-        {skills.map((skill, index) => (
-          <div key={index} className="skill-card">
-            <p aria-placeholder='icon' className='skill-icon'>{skill.icon}</p>
-            <h3>{skill.name}</h3>
-          </div>
-        ))}
+          <h1 className='skillTitle about-title'>Skills/Tools</h1>
+          {skills.map((skill, index) => (
+            <div key={index} className="skill-card">
+              <p aria-placeholder='icon' className='skill-icon'>{skill.icon}</p>
+              <h3>{skill.name}</h3>
+            </div>
+          ))}
         </div>
 
         <div className='skill-right'>
-        <h1 className='skillTitle about-title'>Tools</h1>
-        {tools.map((tool, index) => (
-          <div key={index} className="skill-card">
-            <p aria-placeholder='icon' className='skill-icon'>{tool.icon}</p>
-            <h3>{tool.name}</h3>
+          <div className='flex gap-3 items-center justify-center'>
+            <a
+              className='map-title mt-3'
+              href={`https://github.com/Pratyush-Ge`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              GitHub
+            </a> <FaExternalLinkAlt />
           </div>
-        ))}
+
+          <Profile username='Pratyush-Ge' />
+
+          <div className='flex gap-3 items-center justify-center flex-col mb-3'>
+            <div className='flex gap-3 items-center justify-center'>
+            <a
+              className='map-title mt-3'
+              href={`https://leetcode.com/u/herewegoo/`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Leetcode
+            </a> <FaExternalLinkAlt />
+            </div>
+            <p className='text-center'>500+ Questions on problem solving and DSA. </p>
+          </div>
         </div>
       </div>
     </section>
